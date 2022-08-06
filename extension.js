f7d7db1cc3d317adeab6531b02ae2ba6d1371de9
dan <dan@teixweb.com>
2022-08-06 15:33:25 +0200

    merged fterrag-master

--- extension.js
+++ extension.js
@@ -80,145 +80,164 @@ function makeArgs(toolPath, configPath, filePath) {
         args.push('--allow-risky=yes');
     }
 
-    if (configPath) {
-        args.push('--config=' + configPath);
+    if (getConfig('intersection')) {
+        args.push('--path-mode=intersection');
     }
 
-    if (!configPath) {
-        // log("config file not found. adding rules")
-        let rules = getConfig('rules');
-        if (rules) {
-            args.push('--rules=' + rules);
-        }
-    }
-
-    args.push(filePath);
-
-    return args;
-}
-
-function getConfigFile(basePath) {
-    let fileNames = getConfig('config').split(',');
-    let configFile;
-    try {
-        configFile = getFilePath(fileNames, basePath);
-    } catch (e) {
-        vscode.window.showErrorMessage(e);
-    }
-    return configFile;
-}
-
-function getToolPath(basePath) {
-    const defaultPath = vscode.extensions.getExtension('fterrag.vscode-php-cs-fixer').extensionPath + '/php-cs-fixer';
-    let pathConfig = getConfig('toolPath');
-
-    if (!pathConfig) {
-        return defaultPath;
-    }
-
-    let toolPaths = pathConfig.split(',');
-    let toolPath = getFilePath(toolPaths, basePath);
-
-    if (!toolPath) {
-        return defaultPath;
-    }
-
-    return toolPath;
-}
-
-
-function absoluteExists(filePath) {
-    return path.isAbsolute(filePath) && fs.existsSync(filePath);
-}
-
-/**
- * finds if any of filenames exists in basePath and parent directories
- * and returns the path
- * @param {array} fileNames 
- * @param {string} basePath 
- * @returns string | undefined
- */
-function getFilePath(fileNames, basePath) {
-    if (fileNames.length === 0) {
-        return undefined;
-    }
-
-    let currentPath;
-    let currentFile;
-    let triedPaths;
-    let foundPath;
-
-    for (let i = 0; i < fileNames.length; i++) {
-        currentFile = fileNames[i];
-
-        // log(currentFile);
-        if (absoluteExists(currentFile)) {
-            // log('found absolute');
-            return currentFile;
+    let config = getConfig('config');
+    if (config) {
+        // Support config file with relative path
+        if (!path.isAbsolute(config)) {
+            let currentPath = opts.cwd;
+            let triedPaths = [currentPath];
+            while (!fs.existsSync(currentPath + path.sep + config)) {
+                let lastPath = currentPath;
+                currentPath = path.dirname(currentPath);
+                if (lastPath == currentPath) {
+                    vscode.window.showErrorMessage(`Unable to find ${config} file in ${triedPaths.join(", ")}`);
+                    return;
+                } else {
+                    triedPaths.push(currentPath);
+                }
+            }
+            config = currentPath + path.sep + config;
         }
 
-        currentPath = basePath;
-        triedPaths = [currentPath];
-        while (!fs.existsSync(currentPath + path.sep + currentFile)) {
-            let lastPath = currentPath;
-            currentPath = path.resolve(currentPath, '..');
-            // log(currentPath);
-            // log(lastPath + ":" + currentPath);
-            if (lastPath === currentPath) {
-                // log('not found');
-                break;
-            } else {
-                triedPaths.push(currentPath);
+        if (!configPath) {
+            // log("config file not found. adding rules")
+            let rules = getConfig('rules');
+            if (rules) {
+                args.push('--rules=' + rules);
             }
         }
 
-        foundPath = currentPath + path.sep + currentFile;
-        // log(foundPath);
-        if (fs.existsSync(foundPath)) {
-            // log('really found ' + foundPath);
-            return foundPath;
+        args.push(filePath);
+
+        return args;
+    }
+
+    function getConfigFile(basePath) {
+        let fileNames = getConfig('config').split(',');
+        let configFile;
+        try {
+            configFile = getFilePath(fileNames, basePath);
+        } catch (e) {
+            vscode.window.showErrorMessage(e);
         }
-    };
+        return configFile;
+    }
 
-    return undefined;
-}
+    function getToolPath(basePath) {
+        const defaultPath = vscode.extensions.getExtension('fterrag.vscode-php-cs-fixer').extensionPath + '/php-cs-fixer';
+        let pathConfig = getConfig('toolPath');
 
-function registerDocumentProvider(document, options) {
-    return new Promise(function (resolve, reject) {
-        formatDocument(document).then(function (text) {
-            const range = new vscode.Range(new vscode.Position(0, 0), document.lineAt(document.lineCount - 1).range.end);
-            resolve([new vscode.TextEdit(range, text)]);
-        }).catch(function (err) {
-            reject();
+        if (!pathConfig) {
+            return defaultPath;
+        }
+
+        let toolPaths = pathConfig.split(',');
+        let toolPath = getFilePath(toolPaths, basePath);
+
+        if (!toolPath) {
+            return defaultPath;
+        }
+
+        return toolPath;
+    }
+
+
+    function absoluteExists(filePath) {
+        return path.isAbsolute(filePath) && fs.existsSync(filePath);
+    }
+
+    /**
+     * finds if any of filenames exists in basePath and parent directories
+     * and returns the path
+     * @param {array} fileNames 
+     * @param {string} basePath 
+     * @returns string | undefined
+     */
+    function getFilePath(fileNames, basePath) {
+        if (fileNames.length === 0) {
+            return undefined;
+        }
+
+        let currentPath;
+        let currentFile;
+        let triedPaths;
+        let foundPath;
+
+        for (let i = 0; i < fileNames.length; i++) {
+            currentFile = fileNames[i];
+
+            // log(currentFile);
+            if (absoluteExists(currentFile)) {
+                // log('found absolute');
+                return currentFile;
+            }
+
+            currentPath = basePath;
+            triedPaths = [currentPath];
+            while (!fs.existsSync(currentPath + path.sep + currentFile)) {
+                let lastPath = currentPath;
+                currentPath = path.resolve(currentPath, '..');
+                // log(currentPath);
+                // log(lastPath + ":" + currentPath);
+                if (lastPath === currentPath) {
+                    // log('not found');
+                    break;
+                } else {
+                    triedPaths.push(currentPath);
+                }
+            }
+
+            foundPath = currentPath + path.sep + currentFile;
+            // log(foundPath);
+            if (fs.existsSync(foundPath)) {
+                // log('really found ' + foundPath);
+                return foundPath;
+            }
+        };
+
+        return undefined;
+    }
+
+    function registerDocumentProvider(document, options) {
+        return new Promise(function (resolve, reject) {
+            formatDocument(document).then(function (text) {
+                const range = new vscode.Range(new vscode.Position(0, 0), document.lineAt(document.lineCount - 1).range.end);
+                resolve([new vscode.TextEdit(range, text)]);
+            }).catch(function (err) {
+                reject();
+            });
         });
-    });
-}
+    }
 
-function getConfig(key) {
-    return vscode.workspace.getConfiguration('vscode-php-cs-fixer').get(key);
-}
+    function getConfig(key) {
+        return vscode.workspace.getConfiguration('vscode-php-cs-fixer').get(key);
+    }
 
-function activate(context) {
-    context.subscriptions.push(vscode.commands.registerTextEditorCommand('vscode-php-cs-fixer.fix', function (textEditor) {
-        vscode.commands.executeCommand('editor.action.formatDocument');
-    }));
+    function activate(context) {
+        context.subscriptions.push(vscode.commands.registerTextEditorCommand('vscode-php-cs-fixer.fix', function (textEditor) {
+            vscode.commands.executeCommand('editor.action.formatDocument');
+        }));
 
-    context.subscriptions.push(vscode.workspace.onWillSaveTextDocument(function (event) {
-        if (event.document.languageId === 'php' && getConfig('fixOnSave') && vscode.workspace.getConfiguration('editor', null).get('formatOnSave') == false) {
-            event.waitUntil(vscode.commands.executeCommand('editor.action.formatDocument'));
-        }
-    }));
+        context.subscriptions.push(vscode.workspace.onWillSaveTextDocument(function (event) {
+            if (event.document.languageId === 'php' && getConfig('fixOnSave') && vscode.workspace.getConfiguration('editor', null).get('formatOnSave') == false) {
+                event.waitUntil(vscode.commands.executeCommand('editor.action.formatDocument'));
+            }
+        }));
 
-    context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider('php', {
-        provideDocumentFormattingEdits: function (document, options) {
-            return registerDocumentProvider(document, options);
-        }
-    }));
-}
+        context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider('php', {
+            provideDocumentFormattingEdits: function (document, options) {
+                return registerDocumentProvider(document, options);
+            }
+        }));
+    }
 
-exports.activate = activate;
+    exports.activate = activate;
 
-function deactivate() {
-}
+    function deactivate() {
+    }
 
-exports.deactivate = deactivate;
+    exports.deactivate = deactivate;
